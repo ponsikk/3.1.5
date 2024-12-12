bfa89fa618c23b34799372b29db3b8141e8199d0
Ilya <ponsik98@mail.ru>
2024-12-12 13:10:17 +0300

    Исправлены названия

--- src/main/java/com/example/demo/controller/AdminRestController.java
+++ src/main/java/com/example/demo/controller/AdminRestController.java
@@ -23,14 +23,15 @@ import java.util.List;
 @EnableAutoConfiguration
 public class AdminRestController {
 
-    private UserService userService;
-    private RoleService roleService;
+    private final UserService userService;
+    private final RoleService roleService;
 
     @Autowired
     public AdminRestController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
+
 
     @GetMapping
     public ModelAndView showAdminPage(Principal principal) {
@@ -41,38 +42,45 @@ public class AdminRestController {
     }
 
 
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> showAllUsers() {
-        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    @GetMapping("/get-all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
 
-    @PostMapping(value = "/users")
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
+    @GetMapping("/get/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        User user = userService.findById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
         userService.addUser(user);
         return ResponseEntity.ok(user);
     }
 
-    @PutMapping(value = "/users")
+
+    @PutMapping("/update")
     public ResponseEntity<User> updateUser(@RequestBody User user) {
         userService.updateUser(user);
         return ResponseEntity.ok(user);
     }
 
-    @DeleteMapping(value = "/users/{id}")
+
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable int id) {
         User user = userService.findById(id);
         userService.deleteUser(user);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/users/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable int id) {
-        return new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
-    }
 
     @GetMapping("/roles")
-    public List<Role> showAllRoles() {
-        return roleService.getRoles();
+    public ResponseEntity<List<Role>> getAllRoles() {
+        List<Role> roles = roleService.getRoles();
+        return new ResponseEntity<>(roles, HttpStatus.OK);
     }
 }
